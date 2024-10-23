73282dd7a09e300f4e7da111d9ecdfe70c2b146d
KDA-sparkle <125574085+KDA-sparkle@users.noreply.github.com>
2024-10-23 13:30:10 +0200

    modification du app.jx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Accueil/Accueil";
-import Apropos from "./Pages/Apropos/Apropos";
-import Logement from "./Pages/Logement/Logement";
-import Error404 from "./Pages/Error404/Error404";
-import Header from "./Layout/Header/Header";
-import Footer from "./Layout/Footer/Footer";
+import Gallery from "./components/Gallery/Gallery";
+import Card from "./components/Card/Card";
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/apropos" element={<Apropos />} />
-        <Route path="/logement/:id" element={<Logement />} />
-        <Route path="*" element={<Error404 />} />
+        <Route path="/" element={<Gallery />} />
+        <Route path="/Logement/:id" element={<Card />} />
       </Routes>
-      <Footer />
     </Router>
   );
-}
+};
 
 export default App;
