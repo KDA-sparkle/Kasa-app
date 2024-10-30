b56e030ae33b748bfe984d75f45503dc5263802a
KDA-sparkle <125574085+KDA-sparkle@users.noreply.github.com>
2024-10-30 13:25:17 +0100

    correction de error 404

--- src/Pages/Error404.jsx
+++ src/Pages/Error404.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-function Error404() {
-  return <div></div>;
-}
+const Error = () => {
+  return (
+    <div>
+      <section className="error">
+        <h1 className="error__title">404</h1>
+        <h2 className="error__subtitle">
+          Oups! La page que vous demandez n'existe pas.
+        </h2>
+        <NavLink className="error__link" to="/">
+          Retourner sur la page d’accueil
+        </NavLink>
+      </section>
+    </div>
+  );
+};
 
-export default Error404;
+export default Error;
